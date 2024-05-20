f117562408d2cdca8fcfbf76fe0128110d0aa54c
Jialin <mr.liujalo@gmail.com>
2024-05-20 16:22:19 +0800

    delete test files

--- testprocess/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-gawk 'BEGIN{FS=OFS="\t"} NR==FNR{arr[$1,$3]=substr($0, index($0,$4)); next} {print $0, arr[$1,$3]}' "sort_file1.tsv" "sort_file2.tsv" > test.tsv
-
-gawk 'BEGIN{FS=OFS="\t"} NR==FNR{arr[$1,$3]=$0; next} {print $0, arr[$1,$3]}' "sort_file1.tsv" "sort_file2.tsv" > test.tsv
